b911bd599ab716454cd2c505a91cd93333806e50
Crowdbotics <team@crowdbotics.com>
2021-10-13 09:36:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "steep_frog_31377",
+  url: "https://steep_frog_31377.botics.co",
+  api: "https://steep_frog_31377.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
